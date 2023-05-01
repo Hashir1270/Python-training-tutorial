a772aed31849f89927ff3a656f8135bedb9964bd
Hashir1270 <115805648+Hashir1270@users.noreply.github.com>
2023-05-01 21:34:09 +0500

    Python chapter 1
    Python chapter 1 code by practising clever programmer youtube video python tutorial

---
+++ Pyhton Chapter 1.py
@@ -0,0 +1,39 @@
+#print("Hello World")
+#name = "Hashir Ahmed"
+#age = 21
+#print(name)
+#print(age)
+#full_name = "M.Hashir Ahmed"
+#print(full_name)
+# widht, height = 400, 500
+# print(widht)
+# print(height)
+
+# your_name = input( 'Enter your name: ')
+# print('Hi' + your_name)
+# num1 = input('Enter a number: ')
+# num2 = input('Enter a number: ')
+# print(num1, num2)
+# print(int(num1) + int(num2))
+
+# Data type
+# strings
+# int, float(numerber)
+
+# strings, 'hello', 'Cookies' => "hellocookies"
+# strings, '10', '20' => "1020"
+# Tip App Calculator
+food_amount = float(input("Enter food amount $: "))
+tip_percentage = float(input("Enter food percentage %: ")) / 100
+tip_amount = food_amount * tip_percentage
+print(tip_amount)
+print('\n\n\n')
+print('-----------------------------')
+print(f' 🍛food Amount: ${food_amount}')
+print(f' ⚖tip Amount: ${tip_amount}')
+print('\n')
+# total
+total_amount = tip_amount + food_amount
+print(f' 💵Total Amount: ${total_amount}')
+print('-----------------------------')
+# Chapter 1 End.
