5db0efbe3a76263da47fb62997c2b6241dbf703c
Hashir1270 <115805648+Hashir1270@users.noreply.github.com>
2023-05-04 21:27:12 +0500

    Chapter 02: Boolean

---
+++ main.py
@@ -0,0 +1,62 @@
+# Chapter 02: "Bollean"
+# Lesson 01: "if Else statement"
+# Lesson 02: Boolean
+# lesson 03: Action with variables
+# lesson 04: Grading app
+# IF THEN ELSE
+#  don't allow withdraw
+# else:
+#    allow withdraw
+
+# baby weather app
+# if it's raining outside, grab an umbrella
+# otherwise, grap your sunglasses
+
+# weather = input("how the weather of Today? ")
+
+# if weather == 'rain':
+#  print('☔')
+# elif weather == 'thunderstorm':
+#  print('⛈')
+# elif weather == 'cloudy':
+#  print('🌥')
+# else:
+ # print('😎')
+
+# comparision operators (==, >, <, <=, >=)
+
+# score = 112
+#if score >=90:
+#  print('A+')
+# elif score >=80:
+#  print('A-')
+# elif score >=70:
+#  print('A')
+# elif score >=60:
+#  print('B')
+# elif score >=50:
+#  print('C')
+# elif score >=40:
+#  print('P')
+# elif score >=30:
+#  print('F')
+# elif score >=20:
+#  print('F')
+# elif score >=10:
+#  print('F')
+# else:
+#  print('F')
+
+# either a SUPER Pass passing grade or failing grade
+# SUPER Pass >= 100
+
+score = 67
+# if score >= 60 and score <= 100:
+#   print("passing grade")
+if score >= 60 and score <= 100:
+   print("passing grade")
+  
+if 60 <= score >= 100:
+   print("passing grade")
+if score > 60 or score < 100:
+  print('you either fail or super pass')
